f526d3576c8d570d83b60c596922a87618c30bf0
Munchyy <max.pickup94@gmail.com>
2016-01-12 15:25:44 +0000

    Create Book.cs

---
+++ Book.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOKata
+{
+    /// <summary>
+    /// A class to hold the information about each book.
+    /// This is only price, and the number of the book, in this case 1-5 of the harry potter series.
+    /// If this exercise is extended, this could include name, author, ISBN etc.
+    /// </summary>
+    class Book
+    {
+        private float price;
+        public float Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        //Book number in the harry potter series
+        //1-5
+        private int bookNumber;
+        public int BookNumber
+        {
+            get { return bookNumber; }
+        }
+        
+        public Book(int bookNum, float price)
+        {
+            bookNumber = bookNum;
+            this.price = price;
+        }
+
+        
+    }
+}
